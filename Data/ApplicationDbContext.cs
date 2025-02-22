dd01ecda77b3fde9fffdc870e2424ab578b9f835
xsagatof <Ultimate123443@gmail.com>
2025-02-22 23:00:30 +0500

    Added Database manangement via EF Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<User>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Book> Books { get; set; }
+	}
+}
